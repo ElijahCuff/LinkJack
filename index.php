561def5ce4675d865bd7b3ec8e8eb0f83b8340fb
Venomo <62396650+WokeWorld@users.noreply.github.com>
2020-10-27 10:12:40 +1100

    Update index.php
    updated ip grabber

--- index.php
+++ index.php
@@ -146,7 +146,28 @@ function redirect($url)
 
 function getIp() 
 {
-   return (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:$_SERVER['REMOTE_ADDR']);
+   $ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:getUserIpAddr());
+   if (strlen($ip) > 1)
+   {
+     return $ip;
+   }
+   else
+   {
+     return $_ENV['HTTP_X_FORWARDED_FOR'];
+   }
+}
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
 }
 
 function getStats($id, $key)
